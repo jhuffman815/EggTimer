a200cb5c4201caf5f05fdba00fd11d413670a5c5
Jewell <jhuffman815@gmail.com>
2020-09-01 17:16:14 -0400

    First commit

--- EggTimer/SceneDelegate.swift
+++ EggTimer/SceneDelegate.swift
@@ -2,8 +2,7 @@
 //  SceneDelegate.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Jewell Huffman on 08/07/2019.
 //
 
 import UIKit
--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,14 +2,60 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Jewell Huffman on 08/07/2019.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+     var player: AVAudioPlayer!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
 
+    @IBAction func hardnesSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle
+        
+        secondsRemaining = eggTimes[hardness!]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            timerLabel.text = "\(timeFormatted(secondsRemaining))"
+            secondsRemaining -= 1
+        }
+        else {
+            timer.invalidate()
+            timerLabel.text = "DONE!"
+            playSound(soundName: "alarm")
+        }
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        //     let hours: Int = totalSeconds / 3600
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 
+    
 }
